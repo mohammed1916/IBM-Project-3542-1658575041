d3f89c19b4939726918467d5d4df6d48be070b19
Mohammed Abdullah <63586305+mohammed1916@users.noreply.github.com>
2022-11-19 23:19:01 +0530

    Update make_prediction.py

--- Final Deliverables/Application/make_prediction.py
+++ Final Deliverables/Application/make_prediction.py
@@ -1,6 +1,7 @@
 from tensorflow.keras.utils import load_img
 from tensorflow.keras.utils import img_to_array
 from keras.models import load_model
+from numpy import argmax
 
 
 def argmax(a):
@@ -8,19 +9,18 @@ def argmax(a):
 
 
 def load_image(filename):
-    test_img = load_img(filename, grayscale=True, target_size=(28, 28))
+    test_img = load_img(filename, color_mode = "grayscale", target_size=(28, 28))
     img = img_to_array(test_img)
     img = img.reshape(1, 28, 28, 1)
     img = img.astype('float32')
     img = img / 255.0
-    return img, test_img
+    return img
 
 
 def predict():
-    img, test_img = load_image('sample_image-768x763.png')
+    img = load_image('./images/sample.png')
     model = load_model('model/model.h5')
 
     predict_value = model.predict(img)
     digit = argmax(predict_value)
-    # print(digit)
-    return predict_value
+    return digit
